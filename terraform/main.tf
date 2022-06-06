f1a0ecd704f48b5db6bf60d994cd7199e481d1e8
rrachitha <rrajagopal@architech.ca>
2022-06-06 09:16:37 -0400

    Add more accounts for adigital

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,164 @@
+## Log & Audit
+module "log-audit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = ""
+    AccountName               = "Log & Audit"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "Log"
+    SSOUserLastName           = "Audit"
+  }
+
+  account_tags = {
+    "Log & Audit" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Log & Audit"
+}
+
+## Infra
+module "infra" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = ""
+    AccountName               = "Infra"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "Infra"
+    SSOUserLastName           = "Acc"
+  }
+
+  account_tags = {
+    "Infra" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Infra"
+}
+
+## SDLC DEV
+module "sdlc-dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = ""
+    AccountName               = "SDLCDEV"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "SDLC"
+    SSOUserLastName           = "Dev"
+  }
+
+  account_tags = {
+    "SDLCDEV" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "SDLCDEV"
+}
+
+## SDLC Stage
+module "sdlc-stage" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = ""
+    AccountName               = "SDLCSTAGE"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "SDLC"
+    SSOUserLastName           = "Staging"
+  }
+
+  account_tags = {
+    "SDLCSTAGE" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "SDLCSTAGE"
+}
+
+## Workloads Prod
+module "workloads-prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = ""
+    AccountName               = "Workloads prod"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "workloads"
+    SSOUserLastName           = "prod"
+  }
+
+  account_tags = {
+    "workloads_prod" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "workloads prod"
+}
+
+
+##  Sandbox
 module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = ""
+    AccountName               = "sandbox"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "sandbox"
+    SSOUserLastName           = "dev"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "sandbox dev" = "AFT"
   }
 
   change_management_parameters = {
